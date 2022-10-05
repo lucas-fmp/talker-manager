9a8f43c0bb5d7bc7160273cd3ce89124f22d6138
Lucas Fernando <lucasfernandomacedo13@gmail.com>
2022-10-05 15:24:09 -0300

    requirement 04

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -1,15 +1,31 @@
 const express = require('express');
-const talker = require('../talker');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  const requiredProperties = ['email', 'password'];
-  if (requiredProperties.every((property) => property in req.body)) {
-    const token = (Math.random().toString(16).substr(2) + Math.random().toString(16).substr(2))
-      .slice(0, 16);
-    return res.status(200).json({ token });
+const validateLoginData = (req, res, next) => {
+  const { email, password } = req.body;
+  const regexValidateEmail = /\S+@\S+\.\S+/;
+
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
+  if (!regexValidateEmail.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    console.log(password.length);
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  } 
+    next();
+};
+
+router.post('/', validateLoginData, (req, res) => {
+  const token = (Math.random().toString(16).substr(2) + Math.random().toString(16).substr(2))
+    .slice(0, 16);
+  return res.status(200).json({ token });
 });
 
 module.exports = router;
